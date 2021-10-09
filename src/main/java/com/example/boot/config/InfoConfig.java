1abd8e08000a9f3fa792b630efa28854c044f40d
VanRar <vitalikf7@gmail.com>
2021-10-09 17:29:56 +0300

    дополнено ДЗ

--- src/main/java/com/example/boot/config/InfoConfig.java
+++ src/main/java/com/example/boot/config/InfoConfig.java
@@ -1,25 +1,23 @@
 package com.example.boot.config;
 
+import com.example.boot.profile.DevProfile;
+import com.example.boot.profile.ProductionProfile;
+import com.example.boot.profile.SystemProfile;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class InfoConfig {
-
-    //создаем бины, можем их использовать, например, в контроллере
     @Bean
-    // @Primary //позволяет поставить приоритет, если есть несколько бинов одного типа, то при конфликтах брать этот параметр
-    //можем использовать
-    @ConditionalOnProperty(name = "boot.enabled", havingValue = "true") //если два бина, то лучше использовать true//false, если больше то перечисление
-                                                                        //можно дополнить matchIfMissing = true - если в проверти не прописано, то будет использовано
-    public String name(){
-        return "Ivan";
+    @ConditionalOnProperty(value = "boot.profile.dev", havingValue = "true", matchIfMissing = true)
+    public SystemProfile devProfile() {
+        return new DevProfile();
     }
 
     @Bean
-    public String anotherName(){
-        return "Another name";
+    @ConditionalOnProperty(value = "boot.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
     }
 }
--- src/main/java/com/example/boot/controller/Controller.java
+++ src/main/java/com/example/boot/controller/Controller.java
@@ -1,32 +1,21 @@
 package com.example.boot.controller;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
+import com.example.boot.profile.SystemProfile;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//сразу добавляем аннотацию, что бы Spring нашел этот класс
 @RestController
+@RequestMapping("/")
 public class Controller {
-    /*
-    @Value("${boot.username}")
-    private String username;//берет значения по умолчанию из проперти, либо можем дефолтное указать здесь, например вот так "${boot.username:Oleg}"
-    после создания класса User, реализация уже будет такой:
-     */
-    //private User user;
-    private String user;
-//так ак есть аннотация и конструктор один, то спринг инжектирует сюда бин
+    private SystemProfile profile;
 
-    //с инфоКонфиг:
-    public Controller(String user) { //Ошибку идея не выдала, только при компиляйии, по факту два бина, указываем конкретный
-        // @Qualifier("bv")
-        this.user = user;
+    public Controller(SystemProfile profile) {
+        this.profile = profile;
     }
 
-
-
-    @GetMapping("/hello")  //когда будет задан запрос на адрес/hello он будет обработан здесь
-    public String greeting(){
-        return "Hello, " + user + ", from SpringBootApp ! Your pass is";
+    @GetMapping("profile")
+    public String getProfile() {
+        return profile.getProfile();
     }
 }
